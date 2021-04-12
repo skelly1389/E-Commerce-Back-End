94a7ba790e223129b08808ace52540bbcfdc75df
Sean Kelly <invertedrdm@gmail.com>
2021-04-12 17:25:11 -0400

    tag fetch all seems to work, or at least returns a list of tags and items inside

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,10 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-// router.get('/', (req, res) => {
-//   // find all products
-//   // be sure to include its associated Category and Tag data
-// });
+router.get('/', async (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, as: 'tags'}, { model: ProductTag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // get one product
 // router.get('/:id', (req, res) => {
