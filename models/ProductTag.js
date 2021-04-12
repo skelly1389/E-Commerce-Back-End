a2f9f78736edc8d54ea6718436bf76255736e90f
Sean Kelly <invertedrdm@gmail.com>
2021-04-12 00:13:41 -0400

    started sorting through the models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,18 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_id: {
+      
+    // },
+    // tag_id: {
+
+    // }
   },
   {
     sequelize,
