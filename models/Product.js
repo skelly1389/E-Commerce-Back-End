e53a5e3ccc6c6bf7478843d7b37fa7647a8bc7a9
Sean Kelly <invertedrdm@gmail.com>
2021-04-12 16:30:12 -0400

    seeds now work, models appear to function

--- models/Product.js
+++ models/Product.js
@@ -34,9 +34,13 @@ Product.init(
         isNumeric: true
       }
     },
-    // category_id: {
-
-    // }
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -12,9 +12,9 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    // tag_name: {
-      
-    // }
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
